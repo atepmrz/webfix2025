934949c1cac39d83dac6ccc98d6a0a8fa38d1a1a
Ramon Shabo <ramonwin@programmer.net>
2025-07-09 09:19:37 +0700

    fix pagi

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -24,6 +24,7 @@
         /* Adjust height automatically */
     }
 </style>
+
 <!-- Carousel Start -->
 <div class="container-fluid px-0">
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
